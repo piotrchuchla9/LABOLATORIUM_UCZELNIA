45cd00ca1746d2396ea0b32a23e1f3144e238c94
Piotr Chuchla <piotrchuchla9@gmail.com>
2020-04-30 18:58:19 +0200

    LAB8 zaczete

---
+++ src/main/java/LAB8/matematyka/UlamekZwykly.java
@@ -0,0 +1,20 @@
+package LAB8.matematyka;
+
+public class UlamekZwykly {
+
+    float licznik;
+    float mianownik;
+
+    static int ileUlamkow;
+    static final String kreska = "/";
+
+    public UlamekZwykly(float licznik,float mianownik){
+        this.licznik = licznik;
+        this.mianownik = mianownik;
+    }
+
+    public static void wyswietl(){
+
+    }
+
+}
---
+++ src/main/java/LAB8/matematyka/UlamekZwyklyMain.java
@@ -0,0 +1,9 @@
+package LAB8.matematyka;
+
+public class UlamekZwyklyMain {
+    public static void main(String[] args) {
+
+
+
+    }
+}
